2684a53ec3d2b64951c06a23c827a034221176ba
이아현 <lah1203@naver.com>
2022-04-18 00:48:25 +0900

    1~13 풀이 업로드

---
+++ 11.ts
@@ -0,0 +1,7 @@
+declare module 'str-utils' {
+  export function strReverse(str: string): string;
+  export function strToLower(str: string): string;
+  export function strToUpper(str: string): string;
+  export function strRandomize(str: string): string;
+  export function strInvertCase(str: string): string;
+}
